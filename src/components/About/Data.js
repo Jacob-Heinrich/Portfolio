6746cd88e17cae0d3c5e98b16405d0f7dd5dc166
Jacob <jacob.heinrich09@gmail.com>
2022-06-19 15:18:00 -0600

    Updated about me paragraph

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -5,7 +5,7 @@ export const homeObjOne = {
   lightTextDesc: true,
   topLine: "About me",
   headline: "Hi, I'm Jacob",
-  description: "I'm a Software Developer based out of Boulder, CO with a passion for learning and building software. After 8 years of working on F-16 aircraft I decided to switch careers to follow this passion which led me to attending Oregon State University for a BSc in Computer Science which has helped me gain a strong foundation in design, development, and testing of software.",
+  description: "I'm a Software Developer based out of Boulder, CO with a passion for learning and building software. After 8 years of working on F-16 aircraft I decided to switch careers to follow this passion which led me to attending Oregon State University for a BSc in Computer Science which helped me gain a strong foundation in design, development, and testing of software.",
   imgStart: false,
   img: require('../../images/background-img.jpg'),
   dark: true,
